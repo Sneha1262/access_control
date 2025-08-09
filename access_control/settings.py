b4059aedc14ae3ae1155095a2674fc65946d87c7
Sneha1262 <sne6124@gmail.com>
2025-08-09 13:50:49 +0100

    changes

--- access_control/settings.py
+++ access_control/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,9 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://44.218.129.24:8000",
    
     " http://127.0.0.1:8000/",  # example for frontend dev
 ]
 
-ALLOWED_HOSTS = ['44.218.129.24', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['44.218.129.24', 'localhost', 'http://127.0.0.1:8000/']
