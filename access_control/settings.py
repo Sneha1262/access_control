869d06c53061f23d5a835b9354d4a51e0da38577
Sneha1262 <sne6124@gmail.com>
2025-08-09 13:29:49 +0100

    localhost

--- access_control/settings.py
+++ access_control/settings.py
@@ -126,3 +126,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://44.218.129.24:8000",
+   
+    " http://127.0.0.1:8000/",  # example for frontend dev
+]
+
